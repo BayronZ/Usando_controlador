52f05fa84936aa525e40c0c4b367f09db20b8f66
BayronZ <bayron.zavalag@gmail.com>
2022-06-29 23:45:47 -0400

    refactoritation

--- cart.rb
+++ cart.rb
@@ -1,3 +1,42 @@
 class Cart < ApplicationRecord
     
+    def order_payment(Order)
+        order = Order.find(params[:cart][:order_id])
+        #price must be in cents
+        price = order.total * 100
+        response = EXPRESS_GATEWAY.setup_purchase(price,
+            ip: request.remote_ip,
+            return_url: process_paypal_payment_cart_url,
+            cancel_return_url: root_url,
+            allow_guest_checkout: true,
+            currency: "USD"
+        )
+
+        payment_method = PaymentMethod.find_by(code: "PEC")
+        Payment.create(
+            order_id: order.id,
+            payment_method_id: payment_method.id,
+            state: "processing",
+            total: order.total,
+            token: response.token
+        )
+    end
+
+    def response_state(response)
+        if response.success?
+            payment = Payment.find_by(token: response.token)
+            order = payment.order
+        
+            #update object states
+            payment.state = "completed"
+            order.state = "completed"
+        
+            ActiveRecord::Base.transaction do
+                order.save!
+                payment.save!
+            end
+        end
+    end
+
+
 end
--- carts_controller.rb
+++ carts_controller.rb
@@ -13,26 +13,7 @@ class CartsController < ApplicationController
     end
 
     def pay_with_paypal
-        order = Order.find(params[:cart][:order_id])
-        
-        #price must be in cents
-        price = order.total * 100
-        response = EXPRESS_GATEWAY.setup_purchase(price,
-            ip: request.remote_ip,
-            return_url: process_paypal_payment_cart_url,
-            cancel_return_url: root_url,
-            allow_guest_checkout: true,
-            currency: "USD"
-        )
-        payment_method = PaymentMethod.find_by(code: "PEC")
-        Payment.create(
-            order_id: order.id,
-            payment_method_id: payment_method.id,
-            state: "processing",
-            total: order.total,
-            token: response.token
-        )
-
+        order_payment(Order)
         redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
     end
 
@@ -49,18 +30,6 @@ class CartsController < ApplicationController
         price = details.params["order_total"].to_d * 100
         
         response = EXPRESS_GATEWAY.purchase(price, express_purchase_options)
-        if response.success?
-            payment = Payment.find_by(token: response.token)
-            order = payment.order
-        
-            #update object states
-            payment.state = "completed"
-            order.state = "completed"
-        
-            ActiveRecord::Base.transaction do
-                order.save!
-                payment.save!
-            end
-        end
+        response_state(response)
     end
 end
